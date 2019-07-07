fe0c978deb2dbcf81033deed49bbdec7c88c2691
Alex Weatherley <aweatherley@gmail.com>
2019-07-07 22:33:58 +1000

    2.4 Completed

--- src/MusicalAnt.cpp
+++ src/MusicalAnt.cpp
@@ -986,7 +986,7 @@ struct MusicalAntWidget : ModuleWidget {
 
 	MusicalAntWidget(MusicalAnt *module) {
 		setModule(module);
-		setPanel(SVG::load(assetPlugin(pluginInstance, "res/MusicalAnt.svg")));
+		setPanel(APP->window->loadSvg(assetPlugin(pluginInstance, "res/MusicalAnt.svg")));
 
 		addChild(createWidget<ScrewSilver>(Vec(RACK_GRID_WIDTH, 0)));
 		addChild(createWidget<ScrewSilver>(Vec(box.size.x - 2 * RACK_GRID_WIDTH, 0)));
